51d3d38e33e2d084dc5ba3c534112643be3d1eaf
Iker <eliasarza21@gmail.com>
2024-11-26 15:38:55 +0000

    add

--- src/AES_Encryption.java
+++ src/AES_Encryption.java
@@ -1,3 +1,25 @@
+
+import java.io.IOException;
+
+
 public class AES_Encryption {
+
+
+    public static void EncryptFile(String[] args) throws IOException {
+        String plainTextFileName = "plaintext.txt";
+        String cypherTextFileName = "cyphertext.txt";
+        String decryptedFileName = "plaintextDecrypted.txt";
+        byte[] key = "1234567890".getBytes();
+        encryptWith(plainTextFileName, cypherTextFileName, key);
+        decryptWith(cypherTextFileName, decryptedFileName, key);
+        System.out.println("File used for encryption" + plainTextFileName);
+        System.out.println("Created encryptionFile" + cypherTextFileName);
+        System.out.println("Created decryptionFile" + decryptedFileName);
+
+    }
+
+
+    public void AES_Cypher(String FilenamePlain, String FilenameEnc, byte[] key) throws IOException {
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String correctPassword = "hola";
 
         while (true) {
             System.out.println("Choose an option: \n1. EncryptFile \n2. DecryptFile \nType 'q' to quit.");
@@ -16,32 +14,36 @@ public class Main {
 
             if (input.equals("1")) {
 
-                    System.out.println("Enter password:");
-                    String enteredPassword = scanner.nextLine();
 
-                    if (enteredPassword.equals(correctPassword)) {
-                        System.out.println("Password correct.");
+                System.out.println("Enter a filename to encrypt:");
+                String filename = scanner.nextLine();
+                System.out.println("Enter the Output file name");
+                String outputfile = scanner.nextLine();
+                System.out.println("Enter Shift:");
+                int shift = scanner.nextInt();
+                scanner.nextLine();
 
-                        System.out.println("Enter a filename to encrypt:");
-                        String filename = scanner.nextLine();
-                        System.out.println("Enter the Output file name");
-                        String outputfile = scanner.nextLine();
-                        System.out.println("Enter Shift:");
-                        int shift = scanner.nextInt();
-                        scanner.nextLine();
+                try {
+                    AES_Encryption.EncryptUtil.EncryptFile(filename, outputfile, shift);
+                } catch (Exception e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+                break;
 
-                        try {
-                            EncryptUtil.EncryptFile(filename, outputfile, shift);
-                        } catch (Exception e) {
-                            System.out.println("Error: " + e.getMessage());
-                        }
-                        break;
-                    } else {
-                        System.out.println("Incorrect password. Try again.");
 
+                if (input.equals("2")) {
+
+                    System.out.println("Enter a filename to decrypt:");
+                    filename = scanner.nextLine();
+                    System.out.println("Enter the Output file name");
+                    outputfile = scanner.nextLine();
+                } else {
+                    System.out.println("Invalid input, try again");
+
+                }
             }
-        }
 
-        scanner.close();
+            scanner.close();
+        }
     }
 }
